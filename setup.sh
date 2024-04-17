98889877e3911b5c2f8855811f8daf3ee130ab81
HoangT <123215703+vtangstastic@users.noreply.github.com>
2024-04-17 04:16:08 +0000

    Adding setup.sh from the codespace!

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install sl
+
+#What are dotfiles? 
+#Dotfiles are files and folders on Unix-like systems starting with . that control the configuration of applications and shells on your system. You can store and manage your dotfiles in a repository on GitHub.
